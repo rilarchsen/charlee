d9b8910e7fcbebeef60094a3d32214fb8312f2fe
rilarchsen <rilarchsen@gmail.com>
2022-10-19 09:41:11 +0200

    Added swagger

---
+++ Charlee/Controllers/TestApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Charlee.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TestApiController : Controller
+    {
+        [HttpGet]
+        [Route("/[controller]")]
+        public IActionResult Index()
+        {
+            return Ok("TestApi");
+        }
+
+        /// <summary>
+        /// Returns 200 OK and a greeting including the name provided
+        /// </summary>
+        /// <param name="name">Name which to greet</param>
+        /// <returns>OkObjectResult</returns>
+        [HttpGet]
+        public IActionResult Greet([FromQuery] string name)
+        {
+            return Ok("Hello " + name);
+        }
+    }
+}
--- Charlee/Program.cs
+++ Charlee/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    /*options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        *//*Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",*//*
+    });*/
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
@@ -11,6 +28,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
